f4ca4f3b6a5aac7d7869efa0bdb7efd6fd537406
Mikis78 <mikis78@tlen.pl>
2018-08-24 20:14:47 +0200

    add mvc student

--- src/main/java/com/sda/mvc/Main.java
+++ src/main/java/com/sda/mvc/Main.java
@@ -1,9 +1,26 @@
 package com.sda.mvc;
 
+import javax.swing.text.View;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Student student = new Student(
+                "John",
+                "Novak",
+                "123456");
+
+        StudentView studentView = new StudentView();
+
+        StudentController controller = new StudentController(
+                studentView, student
+        );
+
+        controller.updateView();
+//      student.setName("Other New Name");
+        controller.setStudentName("New Name");
+        controller.updateView();
     }
 
 }
---
+++ src/main/java/com/sda/mvc/Student.java
@@ -0,0 +1,38 @@
+package com.sda.mvc;
+
+public class Student {
+
+    private String name;
+    private String lastName;
+    private String indexNumber;
+
+    public Student(String name, String lastname, String indexNumber) {
+        this.name = name;
+        this.lastName = lastname;
+        this.indexNumber = indexNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIndexNumber() {
+        return indexNumber;
+    }
+
+    public void setIndexNumber(String indexNumber) {
+        this.indexNumber = indexNumber;
+    }
+}
